c562695ac7004e69a5a1b618ace400ffddba6490
hpam1 <hpam1@192.168.2.7>
2016-08-10 11:24:58 -0400

    Made modifications to read the hold expiry threshold from a properties file

--- TicketService/src/main/java/com/walmart/ts/service/HoldValidator.java
+++ TicketService/src/main/java/com/walmart/ts/service/HoldValidator.java
@@ -3,8 +3,15 @@
  */
 package com.walmart.ts.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.walmart.ts.beans.SeatHold;
 import com.walmart.ts.beans.SeatHoldRepository;
@@ -12,23 +19,61 @@ import com.walmart.ts.beans.SeatStatus;
 
 /**
  * @author Haarthi Padmanabhan
+ * 
+ * HoldValidator clears SeatHolds that are older than a threshold value and 
+ * releases the held seats to other users
+ * 
+ * SeatHoldList is a linkedlist whose elements are sorted by the hold creation time
+ * with the element at the head of the list representing the oldest hold and that at
+ * the tail represents the newest hold
+ * Iterate through the seatholdlist from the head till the element whose hold creation
+ * time is within the threshold (in seconds)
+ * 
  *
  */
 public class HoldValidator {
+	static Logger log = LogManager.getLogger(HoldValidator.class);
+	// default threshold time is 60 seconds
+	static long holdThreshold = 60l;
+	/*
+	 * read the hold expiration threshold value from a properties file TSProperties.properties
+	 * located in resources folder
+	 * 
+	 */
+	static {
+		ClassLoader classLoader = HoldValidator.class.getClassLoader();
+		File file = new File(classLoader.getResource("TSProperties.properties").getFile());
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream(file));
+			holdThreshold = Long.parseLong((String) properties.get("HoldExpirationThresholdInSeconds"));
+		} catch (IOException e) {
+			log.error("Error reading TSProperties file for hold threshold " + e.getMessage());
+		}
+		log.info("Hold threshold in seconds: " + holdThreshold);
+	}
 	
+	/*
+	 * clear the holds that are more than threshold seconds from the current time
+	 */
 	public static void purgeExpiredHolds() {
 		LinkedList<SeatHold> seatHoldList = SeatHoldRepository.getSeatHoldList();
+		log.info("Start: In HoldValidator purging expired holds; no of holds: " + seatHoldList.size());
+		// iterate through the hold list
 		while(seatHoldList != null && seatHoldList.size() > 0) {
 			SeatHold sh = seatHoldList.getFirst();
 			LocalDateTime holdTime = sh.getHoldStartTime();
 			LocalDateTime currentTime = LocalDateTime.now();
 			long diffInSeconds = java.time.Duration.between(holdTime, currentTime).getSeconds();
-		    if(diffInSeconds >= 60) {
-		    	sh.getHeldSeatList().forEach(seat -> seat.setStatus(SeatStatus.AVAILABLE));
+			if(diffInSeconds >= holdThreshold) { // hold expired
+				// release the held seats
+				if(sh.getHeldSeatList() != null && sh.getHeldSeatList().size() > 0)
+					sh.getHeldSeatList().forEach(seat -> seat.setStatus(SeatStatus.AVAILABLE));
 		    	seatHoldList.removeFirst();
-		    } else {
+		    } else { // the current element within the threshold and so are the next elements; break the processing
 		    	break;
 		    }
 		}
+		log.info("End: In HoldValidator; no of holds: " + seatHoldList.size());
 	}
 }
