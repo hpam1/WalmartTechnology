6f1e4814afbc615c07055a7642a211482108fae8
hpam1 <hpam1@192.168.2.7>
2016-08-10 11:24:02 -0400

    Added input validation and fixed bug when checking for available seats

--- TicketService/src/main/java/com/walmart/ts/utils/SeatDAOUtil.java
+++ TicketService/src/main/java/com/walmart/ts/utils/SeatDAOUtil.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.walmart.ts.beans.Level;
 import com.walmart.ts.beans.Seat;
 import com.walmart.ts.beans.SeatStatus;
@@ -13,27 +16,78 @@ import com.walmart.ts.beans.Venue;
 /**
  * 
  * @author Haarthi Padmanabhan
+ * 
+ * A utility class for querying about seat and seat holds
  *
  */
 public class SeatDAOUtil {
+	static Logger log = LogManager.getLogger(SeatDAOUtil.class);
+	
+	/*
+	 * count the number of available seats in the venue
+	 * 
+	 *  @param venue - the performance venue
+	 *  @return the count of available seats in the venue
+	 */
 	public static int countAvailSeatInVenue(Venue venue) {
+		log.debug("SeatDAOUtil countAvailSeatInVenue " + venue);
+		if(venue == null)
+			return 0;
 		List<Level> levelList = venue.getVenueLevels();
 		return getAvailSeat(levelList);
 	}
 	
+	/*
+	 * count the number of available seats in a particular venue level
+	 * 
+	 * @param venue - the performance venue
+	 * @param levelId - the venue level id
+	 * @return the count of available seats in the venue
+	 * 
+	 * If a level Id that does not exist in the venue is given
+	 * as input, this method will return 0
+	 * 
+	 */
 	public static int countAvailSeatInLevel(Venue venue, int levelId) {
+		log.debug("SeatDAOUtil countAviailSeatInLevel " + venue + " " + levelId);
+		if(venue == null)
+			return 0;
 		List<Level> levelList = venue.getVenueLevels().stream().filter(lvl -> lvl.getId() == levelId).collect(Collectors.toList());
 		return getAvailSeat(levelList);
 	}
 	
+	/*
+	 * count the number of available seats 
+	 * 
+	 * @param list of venue levels whose available seats are to be computed
+	 * 
+	 */
 	private static int getAvailSeat(List<Level> levelList) {
+		if(levelList == null || levelList.size() == 0)
+			return 0;
 		List<Seat> seatList = levelList.stream().flatMap(lvl -> lvl.getSeatList().stream()).collect(Collectors.toList());
+		if(seatList == null || seatList.size() == 0)
+			return 0;
 		return (int) seatList.stream().filter(seat -> seat.getStatus().equals(SeatStatus.AVAILABLE)).count();
 	}
 	
+	/*
+	 * count the seats available, optionally limited by levels
+	 * If minLevel is present, then search for available seats from this level
+	 * If maxLevel is present, then search for available seats within this level
+	 * otherwise, search the entire venue
+	 *  
+	 */
 	public static int countAvailSeatsWithinLevels(Venue venue, Optional<Integer> minLevel, Optional<Integer> maxLevel) {
+		log.debug("SeatDAOUtil countAvailSeatsWithinLevels");
+		if(venue == null)
+			return 0;
 		List<Level> levelList = venue.getVenueLevels();
+		if(levelList == null || levelList.size() == 0)
+			return 0;
 		if(minLevel.isPresent() && maxLevel.isPresent()) {
+			if(minLevel.get() > maxLevel.get())
+				return 0;
 			levelList = levelList.stream().filter(lvl -> lvl.getId() >= minLevel.get() && lvl.getId() <= maxLevel.get()).collect(Collectors.toList());
 		} else if(minLevel.isPresent()) {
 			levelList = levelList.stream().filter(lvl -> lvl.getId() >= minLevel.get()).collect(Collectors.toList());
@@ -43,22 +97,50 @@ public class SeatDAOUtil {
 		return getAvailSeat(levelList);
 	}
 	
+	/*
+	 * get the available seat list, optionally limited by levels
+	 * If minLevel is present, then search for available seats from this level
+	 * If maxLevel is present, then search for available seats within this level
+	 * otherwise, search the entire venue 
+	 */
 	private static List<Seat> getAvailSeatsInLevels(Venue venue, Optional<Integer> minLevel, Optional<Integer> maxLevel) {
+		if(venue == null)
+			return null;
 		List<Level> levelList = venue.getVenueLevels();
+		if(levelList == null || levelList.size() == 0)
+			return null;
 		if(minLevel.isPresent() && maxLevel.isPresent()) {
+			if(minLevel.get() > maxLevel.get())
+				return null;
 			levelList = levelList.stream().filter(lvl -> lvl.getId() >= minLevel.get() && lvl.getId() <= maxLevel.get()).collect(Collectors.toList());
 		} else if(minLevel.isPresent()) {
 			levelList = levelList.stream().filter(lvl -> lvl.getId() >= minLevel.get()).collect(Collectors.toList());
 		} else if(maxLevel.isPresent()) {
 			levelList = levelList.stream().filter(lvl -> lvl.getId() <= maxLevel.get()).collect(Collectors.toList());
 		}
+		if(levelList == null || levelList.size() == 0)
+			return null;
 		List<Seat> seatList = levelList.stream().flatMap(lvl -> lvl.getSeatList().stream()).collect(Collectors.toList());
+		if(seatList != null && seatList.size() > 0) {
+			seatList = seatList.stream().filter(seat -> seat.getStatus().equals(SeatStatus.AVAILABLE)).collect(Collectors.toList());
+		}
 		return seatList;
 	}
 	
-	public static Seat holdARandomSeat(Venue venue, Optional<Integer> minLevel, Optional<Integer> maxLevel) {
+	/*
+	 * randomly choose an available seat in the venue, optionally limited by levels, and
+	 * hold it for a user.
+	 * 
+	 */
+	public static synchronized Seat holdARandomSeat(Venue venue, Optional<Integer> minLevel, Optional<Integer> maxLevel) {
+		if(venue == null)
+			return null;
 		List<Seat> seatList = getAvailSeatsInLevels(venue, minLevel, maxLevel);
+		if(seatList == null || seatList.size() == 0)
+			return null;
 		Random random = new Random();
-		return seatList.get(random.nextInt(seatList.size()));
+		Seat seat = seatList.get(random.nextInt(seatList.size()));
+		seat.setStatus(SeatStatus.HELD);
+		return seat;
 	}
 }
